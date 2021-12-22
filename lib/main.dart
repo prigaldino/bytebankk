d8c849eece68f222b4b100de17df4eaad403d759
prigaldino <pri_galdino@hotmail.com>
2021-12-22 14:27:51 -0300

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import '../screens/dashboard.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
+
+// ...
+
 
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  FirebaseCrashlytics.instance.setUserIdentifier('alura123');
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  if (kDebugMode) {  
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+
+  } else {
+      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+      FirebaseCrashlytics.instance.setUserIdentifier('alura123');
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  }
 
   runApp(BytebankApp());
 }
